dac6110af16331a63b3da4cb1861e20f318df718
checky <812961937@qq.com>
2019-05-23 18:40:54 +0800

    文件流的延迟释放资源

--- 05.Go语言函数/5.10_延迟执行语句(defer).go
+++ 05.Go语言函数/5.10_延迟执行语句(defer).go
@@ -4,10 +4,11 @@ package main
 // 也就是说，先被 defer 的语句最后被执行，最后被 defer 的语句，最先被执行。
 import (
 	"fmt"
+	"os"
 	"sync"
 )
 
-func deferTest(){
+func deferTest() {
 	fmt.Println("defer begin")
 	// 这行会在最后执行
 	defer fmt.Println(1)
@@ -23,25 +24,72 @@ func deferTest(){
 
 // 1)使用延迟并发解锁
 var (
-
 	valueByKey = make(map[string]int)
 	// 保证使用映射的并发安全的互锁
-	valueByKeyGuard  sync.Mutex
+	valueByKeyGuard sync.Mutex
 )
 
-func readValueNormal(key string)int{
+func readValueNormal(key string) int {
 	valueByKeyGuard.Lock()
-	v:=valueByKey[key]
+	v := valueByKey[key]
 	valueByKeyGuard.Unlock()
 	return v
 }
 
-func readValueByDefer(key string)int{
+func readValueByDefer(key string) int {
 	valueByKeyGuard.Lock()
 	defer valueByKeyGuard.Unlock()
 	return valueByKey[key]
 }
 
-func main()  {
-	deferTest()
+// 2)使用延迟释放文件句柄
+func fileSize(filename string) int64 {
+
+	// 根据文件名打开文件
+	f, err := os.Open(filename)
+	// 如果文件打开错误，返回文件大小为0
+	if err != nil {
+		return 0
+	}
+	// 获取文件状态信息
+	info, err := f.Stat()
+	// 如果获取文件信息错误，关闭文件并返回文件大小为0
+	if err != nil {
+		f.Close()
+		return 0
+	}
+	// 获取文件
+	size := info.Size()
+	f.Close()
+	return size
+}
+
+// 使用defer对代码进行简化
+func fileSizeDefer(filename string) int64 {
+
+	f, err := os.Open(filename)
+
+	if err != nil {
+		return 0
+	}
+
+	defer f.Close()
+
+	info, err := f.Stat()
+	if err != nil {
+		// defer 空中触发，调用关闭文件
+		return 0
+	}
+
+	size := info.Size()
+	// defer 空中触发，调用关闭文件
+	return size
+}
+
+// defer 后的语句（f.Close()）将会在函数返回前被调用，自动释放资源
+
+func main() {
+	//deferTest()
+
+	fmt.Println(fileSizeDefer("test.txt"))
 }
